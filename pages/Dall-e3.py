f85a55d9d52538df9749de11fcb92068fcdccafc
Mateemi <mateo.valentin@eemi.com>
2024-09-26 15:35:50 +0200

    Update Dall-e3.py

--- pages/Dall-e3.py
+++ pages/Dall-e3.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title ("Dall-e3")
 
-st.write ("veuillez entrer une description de l'image que vous souhaitez générer")
+st.write ("Veuillez entrer une description de l'image que vous souhaitez générer")
