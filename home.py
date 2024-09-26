159d7b16427f0c52149f65375b7e7beb5c8e6b62
Mateemi <mateo.valentin@eemi.com>
2024-09-26 14:21:08 +0200

    Update home.py

--- home.py
+++ home.py
@@ -10,3 +10,6 @@ st.write ("Ceci est un formulaire de contact")
 user_input = st.text_input ("Tapez votre texte :")
 
 st.write (user_input)
+
+#Image
+st.image ("https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/a249096c7812deb8c3c2c907173f3774.jpg")
