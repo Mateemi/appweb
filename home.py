c2f331d580e1dfa8b3d5a786505661eb1d8bbde1
Mateemi <mateo.valentin@eemi.com>
2024-09-26 14:28:04 +0200

    Update home.py

--- home.py
+++ home.py
@@ -13,3 +13,6 @@ st.write (user_input)
 
 #Image
 st.image ("https://imgsrv.crunchyroll.com/cdn-cgi/image/fit=contain,format=auto,quality=85,width=1200,height=675/catalog/crunchyroll/a249096c7812deb8c3c2c907173f3774.jpg")
+
+#Sidebar
+st.sidebar.title ("Mateo Valentin")
