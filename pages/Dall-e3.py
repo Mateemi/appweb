fda7083bd80af96ffe03dc879eff0d9ce9b82f61
Mateemi <mateo.valentin@eemi.com>
2024-09-26 15:38:31 +0200

    Update Dall-e3.py

--- pages/Dall-e3.py
+++ pages/Dall-e3.py
@@ -2,4 +2,22 @@ import streamlit as st
 
 st.title ("Dall-e3")
 
-st.write ("Veuillez entrer une description de l'image que vous souhaitez générer")
+user_input : st.text_input ("Open IA")
+st.write(user_input)
+recherche_input : ("Veuillez entrer une description de l'image que vous souhaitez générer")
+st.write(recherche_input)
+
+#clé
+from openai import OpenAI
+st.sidebar
+client = OpenAI(api_key=OpenAIKEY)
+prompt = "A cute baby sea otter"
+image = client.images.generate(
+    model="dall-e-2",
+    prompt=user_input,
+    size="512x512",
+    quality="standard",
+    n=1,
+)
+image_url = image.data[0].url
+print(image_url)
